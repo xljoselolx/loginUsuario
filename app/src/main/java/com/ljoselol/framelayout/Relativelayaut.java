68745d8ad3fdb49bf76ebd13e49f770e38ad8441
xljoselolx <josegurka@gmail.com>
2020-09-27 23:56:17 -0400

    loginUsuario

---
+++ app/src/main/java/com/ljoselol/framelayout/Relativelayaut.java
@@ -0,0 +1,18 @@
+package com.ljoselol.framelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Relativelayaut extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relativelayaut);
+
+    }
+}
